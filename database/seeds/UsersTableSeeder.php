588dceafb1a992a4f4d3af7c9918251a65bf931a
omaralej04 <o_ma_rdiaz@hotmail.com>
2017-02-23 17:08:10 -0400

    Agregar seeds para Usuarios

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,32 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            'nombre' => 'Administrador',
+            'apellido'=>'Sistema',
+            'cedula'=> '0',
+            'fecha_nacimiento' => '1000-01-01',
+            'edad' => '18',
+            'sexo' => 'Hombre',
+            'direccion' => 'Calle 1. Av. 1, Caracas',
+            'email' => 'admin@sistema.com',
+            'password' => bcrypt('az0909az'),
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            'nombre' => 'Default',
+            'apellido'=>'User',
+            'cedula'=> '1',
+            'fecha_nacimiento' => '1000-01-10',
+            'edad' => '20',
+            'sexo' => 'Hombre',
+            'direccion' => 'Calle 2. Av. 3, Caracas',
+            'email' => 'default@user.com',
+            'password' => bcrypt('az0909az'),
+            'created_at' => \Carbon\Carbon::now(),
+            'updated_at' => \Carbon\Carbon::now(),
+        ]);
     }
 }
